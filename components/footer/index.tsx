98aafd1bef42bc2b623c65122ca92a4317e5791b
kavorix <kavorix5@gmail.com>
2024-11-28 05:26:45 +0900

    fix error

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -28,7 +28,7 @@ const Footer = () => {
               display: "inline",
             }}
           />
-          {locale === "en" ? "by Zahra Mirzaei" : "توسط زهرا میرزایی"}
+          {locale === "en" ? "by kavorix" : "توسط زهرا میرزایی"}
         </div>
         <div className="py-1">
           <StartQuot
